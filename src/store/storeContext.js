1843e7abb5b302e2e557c72801234f5ee0878f25
abraham <abrutrenyan049@gmail.com>
2022-01-25 12:13:35 -0300

    se cambiaron nombres y termino con estructura de carpetas

---
+++ src/store/storeContext.js
@@ -0,0 +1,13 @@
+/* import {createContext } from 'react'
+export const Context = createContext(null)
+const UserProvider = ({children}) => {
+return (
+    <Context.Provider value ={5}>
+        {children}
+    </Context.Provider>
+)
+}
+export default UserProvider */ 
+
+
+///Dejo este codigo unicamente para guiarse con la sintaxis del appContext, despues hay que adaptarlo a la logica de nuestro programa.
