9e4daf1d64094af0f7709a3ec677183c9e7f3847
abraham <abrutrenyan049@gmail.com>
2022-01-25 18:46:08 -0300

    se avanzo con el header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,9 @@
 import './Header.css'
 const Header = () => {
     return ( 
-        <div className="fondo-header">
+        <div className="container-header">
             <h1 className='title'>Electro Argentina</h1>
-            <p className='slogan'>Encontrá ése accesorio que estabas buscando, al mejor precio.</p>
+            <p className='slogan'>Encontrá ése dispositivo que estabas buscando, al mejor precio.</p>
             <button className='btn-contact'>Contactanos</button>
         </div>
      );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,32 +1,41 @@
 const Navbar = () => {
     return ( 
-        <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">LOGO CON NOMBRE</a>
+        <div className="row">
+              <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button class="btn btn-outline-success" type="submit">Buscar</button>
+                            <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Mi cuenta</a>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 cuenta_carrito">
+                            <li className="nav-item">
+                                <a className="nav-link active fw-bold" aria-current="page" href="#">Mi cuenta</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <i class="bi bi-cart4"></i>
+                            <li className="nav-item fondoCarrito">
+                                <a className="nav-link" href="#">
+                                    <i className="bi bi-cart4 carrito"></i>
                                 </a>
                             </li>
                         </ul>
-                        
-                    </div>
                 </div>
-            </nav>
-        </>
+            </div>
+        </div>
      );
 }
  
