0563051270a1a5e3ba6f6fd0be8b22dc197e9de3
abraham <abrutrenyan049@gmail.com>
2022-01-25 12:37:43 -0300

    se comenzo a trabajar el componente header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> Rama estuvo aqui otra vez.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
-
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,11 @@
+import './Header.css'
 const Header = () => {
     return ( 
-        <>
-        </>
+        <div className="fondo-header">
+            <h1>Electro Argentina</h1>
+            <p>Encontrá ése accesorio que estabas buscando, al mejor precio.</p>
+            <button>Contactanos</button>
+        </div>
      );
 }
  
