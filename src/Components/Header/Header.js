eb55db3e931c0d3c1d403852fe05b3557f86b2b7
abraham <abrutrenyan049@gmail.com>
2022-01-25 14:39:50 -0300

    prueba pull

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,9 @@ import './Header.css'
 const Header = () => {
     return ( 
         <div className="fondo-header">
-            <h1>Electro Argentina</h1>
-            <p>Encontrá ése accesorio que estabas buscando, al mejor precio.</p>
-            <button>Contactanos</button>
+            <h1 className='title'>Electro Argentina</h1>
+            <p className='slogan'>Encontrá ése accesorio que estabas buscando, al mejor precio.</p>
+            <button className='btn-contact'>Contactanos</button>
         </div>
      );
 }
