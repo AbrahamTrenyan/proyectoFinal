049bbbb658138a2cf2ff596227718bf5b0344d48
Ramixzx <ramiro.mijalovich@gmail.com>
2022-01-25 19:51:31 -0300

    Se avanza con las distintas secciones de la Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,20 @@
+import './Home.css'
 import Header from '../../Components/Header'
+
 const Home = () => {
     return (    
     <>
         <Header />
+
+        <section id="seccion-categorias">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="Imagen de una laptop" />
+                <div class="card-body">
+                    <h5 class="card-title">Laptops</h5>
+                    <p class="card-text text-muted">Apple - Hp - Lenovo</p>
+                </div>
+            </div>
+        </section>
     </>
     );
 }
