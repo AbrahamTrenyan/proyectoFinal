6cb067e0c6ea3722a6dbb4c09608f359ed9017fb
abraham <abrutrenyan049@gmail.com>
2022-01-25 21:55:37 -0300

    se borran comentarios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
-<<<<<<< HEAD
 import Footer from './Components/Footer'
-=======
 import Navbar from './Components/Navbar/Navbar';
 
 
->>>>>>> a287c56db6e5835338688cb1602418cdfa852b5b
 function App() {
   return (
     <>
