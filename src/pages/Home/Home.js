baab6081b64ad9987b7eb0b80d723e8261c874aa
Ramixzx <ramiro.mijalovich@gmail.com>
2022-01-26 20:44:32 -0300

    Casi finalizada la Home, falta hacer responsive el navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -35,10 +35,12 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
             <section id="seccion-ofertas" className="seccion-ofertas">
                 <h2 className="text-center mt-3">Aprovecha nuestras ultimas ofertas!</h2>
             </section>
-            <section id="seccion-info" className="d-flex justify-content-evenly flex-wrap">
+
+            <section id="seccion-info" className="d-flex justify-content-evenly flex-wrap mb-5">
                 <div className="card border-none mt-3" style={{width: '18rem'}}>
                     <i className="bi bi-truck text-center p-2 iconito"></i>
                     <div className="card-body text-center ">
@@ -47,17 +49,50 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="card border-none mt-3" style={{width: '18rem'}}>
-                    <i class="bi bi-patch-check text-center iconito"></i>
+                    <i className="bi bi-patch-check text-center iconito"></i>
                     <div className="card-body text-center ">
                         <h5 className="card-title">Garantia</h5>
                         <p className="card-text text-muted">En caso de querer o necesitar cambiar un producto, el costo del envío por cambio corre por cuenta del cliente(salvo por fallas en el producto), y se efectúa siempre y cuando el producto devuelto se encuentre en su empaque original, sin usar, y con todos sus accesorios.</p>
                     </div>
                 </div>
                 <div className="card border-none mt-3" style={{width: '18rem'}}>
-                    <i class="bi bi-credit-card-2-front text-center iconito"></i>
+                    <i className="bi bi-credit-card-2-front text-center iconito"></i>
                     <div className="card-body text-center ">
                         <h5 className="card-title">Medios de Pago</h5>
                         <p className="card-text text-muted">Aceptamos TODOS los métodos de pago, desde MercadoPago, incluyendo todas las tarjetas de crédito y débito, transferencia, y efectivo en Rapipago o Pago Fácil.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section id="seccion-nosotros/contacto" className="seccion-ofertas">
+                <div className="row ">
+                    <div className="col-md-6 col-sm-12 d-flex flex-wrap justify-content-center align-items-center text-center">
+                        <h2 className="pt-5">Sobre nosotros</h2>
+                        <p className="px-5">Somos dos socios en un emprendimiento con un objetivo muy claro: conseguir los accesorios más demandados del mercado y distribuirlos al mejor precio, por mayor y por menor. 
+                            Nuestros valores, la transparencia, compromiso, y amabilidad con nuestros clientes.
+                            Nuestra visión: competir, crecer, y estar a la altura de las demandas del mercado actual.
+                        </p>
+                    </div>
+                    <div className="col-md-6 col-sm-12 text-start p-5">
+                        <div className="text-center">
+                            <h2>Contactanos</h2>
+                            <h3>Ante cualquier duda no dude en consultar</h3>
+                        </div>
+                        <form action="https://submit-form.com/VoG37Uu9" className="w-75 d-inline">
+                            <div className="mb-3">
+                                <label for="name" className="form-label">Nombre</label>
+                                <input type="text" name="name" className="form-control" id="name" />
+                            </div>
+                            <div className="mb-3">
+                                <label for="email" className="form-label">Email</label>
+                                <input type="email"  name="email" className="form-control" placeholder="ejemplo@gmail.com" id="email" aria-describedby="emailHelp" />
+                            </div>
+                            <div className="mb-3">
+                                <label for="comentarios" className="form-label">Dejanos alguna pregunta o comentario</label>
+                                <textarea className="form-control" name="comentarios" rows="5" cols="10" placeholder="Comentario" id="comentarios" ></textarea>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
                     </div>
                 </div>
             </section>
