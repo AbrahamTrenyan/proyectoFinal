751bafc062d1bba62f67c9d61dd80ad810e4478d
netugithub <netaneldt@gmail.com>
2022-01-26 23:44:32 -0300

    se mostraron las cards de cada producto en su pag

--- src/pages/Audio/Audio.js
+++ src/pages/Audio/Audio.js
@@ -1,6 +1,24 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../store/storeContext";
+import Card from "../../Components/Card";
+
+
+
 const Audio = () => {
+    const { productsList } = useContext(Context)
+    let audio = productsList.filter(product => product.categoria==="audio")
     return ( 
     <>
+        <div className="d-flex flex-wrap justify-content-around m-3">
+            {audio.length > 0 ?
+                audio.map(aud => (
+                    <Card key={aud.id} {...aud} />
+                ))
+                :
+                <p>No tenemos productos</p>
+            }       
+        </div>
+        
     </> 
     );
 }
--- src/pages/Smartwatches/Smartwatches.js
+++ src/pages/Smartwatches/Smartwatches.js
@@ -1,6 +1,23 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../store/storeContext";
+import Card from "../../Components/Card";
+
+
 const Smartwatches = () => {
+    const { productsList } = useContext(Context)
+    let smartwatch = productsList.filter(product => product.categoria==="smartwatch")
     return ( 
     <>
+        <div className="d-flex flex-wrap justify-content-around m-3">
+            {smartwatch.length > 0 ?
+                smartwatch.map(smart => (
+                    <Card key={smart.id} {...smart} />
+                ))
+                :
+                <p>No tenemos productos</p>
+            }       
+        </div>
+        
     </> 
     );
 }
